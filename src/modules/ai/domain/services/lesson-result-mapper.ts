9c7c503c587060eb0b3c1998291016db6033ee46
Venilson Santos <venilson2009@hotmail.com>
2026-03-07 09:36:20 -0300

    chore: delete file.

--- src/modules/ai/domain/services/lesson-result-mapper.ts
+++
@@ -1,6 +0,0 @@
-import { Student } from '@/modules/students/domain/entities/student.entity';
-import { GenerateLessonOutput } from '@/modules/ai/application/use-cases/generate-lesson/generate-lesson.output';
-import { RawAiLesson, RawAiBatchResponse } from '@/modules/ai/application/use-cases/generate-lesson/raw-ai-response.interface';
-
-export class LessonResultMapper {
-}
